3d1c2147172985d527618e77ae3c5ba42cf352b5
Lucas Costa Souza <lucas_costasouza@yahoo.com.br>
2024-02-06 11:46:26 -0300

    Aula 02 - Atividade 05

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import os
 from time import sleep
 from helpers import *
+from selecionar_persona import *
+from selecionar_documento import *
 
 load_dotenv()
 
@@ -13,12 +15,13 @@ modelo = "gpt-4"
 app = Flask(__name__)
 app.secret_key = 'alura'
 
-contexto = carrega("dados/ecomart.txt")
-
 def bot(prompt):
 
     maximo_tentativas = 1
     repeticao = 0
+    personalidade = personas[selecionar_persona(prompt)]
+    contexto = selecionar_contexto(prompt)
+    documento_selecionado = selecionar_documento(contexto)
 
     while True:
         try:
@@ -26,10 +29,14 @@ def bot(prompt):
                 Você é um chatbot de atendimento a clientes de um e-commerce. 
                 Você não deve responder perguntas que não sejam dados do e-commerce informado!
                 
-                Você deve gerar respostas utilizando o contexto abaixo:
+                Você deve gerar respostas utilizando o contexto abaixo.
+                Você deve adotar a persona abaixo.
 
                 # Contexto
-                {contexto}
+                {documento_selecionado}
+
+                # Persona
+                {personalidade}
             """
             response = cliente.chat.completions.create(
                 messages = [
